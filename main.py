6de3a6bc2e37f72b94334b8ab1c86f50e3bd2299
CEREAL <arianmeraz15@gmail.com>
2023-04-04 17:14:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 # IP address of the Starlink router
-url = 'http://192.168.100.1/api/v1/system/advanced/WiFiPassword'
+url = 'http://192.168.1.1/api/v1/system/advanced/WiFiPassword'
 
 # Send request to the router
 response = requests.get(url)
